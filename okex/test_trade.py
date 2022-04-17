d98f37df6b01eb16d9ea6d7e1b337894e10de7f9
jyh <seedjyh@gmail.com>
2022-04-17 21:59:52 +0800

    better trade query

--- okex/test_trade.py
+++ okex/test_trade.py
@@ -1,1 +1,20 @@
 # -*- coding: utf-8 -*-
+import logging
+import sqlite3
+
+from model import Trade
+from okex.trade import set_db_conn, query
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def test_query():
+    # 注意，这个测试只有 2022-04-17 20:58:39 开始没有新交易时才有效。
+    db_conn = sqlite3.connect(database=":memory:")
+    set_db_conn(db_conn)
+    trades = query(last_bill_id="435607391688867841")
+    assert len(trades) == 3
+    assert trades[0].__dict__ == Trade(ccy="xmr", price=233.79, crypto=-0.123668, bill_id='435607463918977025').__dict__
+    assert trades[1].__dict__ == Trade(ccy="xrp", price=0.77057, crypto=-1.039431, bill_id='435607531526963204').__dict__
+    assert trades[2].__dict__ == Trade(ccy="xrp", price=0.77054, crypto=-3.260116, bill_id='435607531526963208').__dict__
--- okex/trade.py
+++ okex/trade.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
+import model
 from okex import _api
+
+_db_conn = None
+
+
+def set_db_conn(db_conn):
+    """
+    设置一个sqlite3的连接，用于缓存数据。
+    :param db_conn:
+    :return:
+    """
+    global _db_conn
+    _db_conn = db_conn
 
 
 def query(last_bill_id: str = None):
@@ -10,7 +23,111 @@ def query(last_bill_id: str = None):
     :param last_bill_id: 上次查询的最终账单号。本次查询从这之后开始。
     :return:
     """
-    trades = _api.query_trade_fills(last_bill_id=last_bill_id)
-    for t in trades:
-        print(t.__dict__)
-    return trades
+    _sync()
+    repo = Repo(db_conn=_db_conn)
+    return repo.query(last_bill_id=last_bill_id)
+
+
+def _sync():
+    """
+    查询本地缓存的交易，并获取从此之后的所有交易。
+    :return:
+    """
+    repo = Repo(db_conn=_db_conn)
+    try:
+        last_bill_id = repo.get_last().bill_id
+    except model.NoSuchRecord:
+        last_bill_id = None
+    for t in _api.query_trade_fills(last_bill_id=last_bill_id):
+        repo.append(t)
+
+
+class Repo:
+    def __init__(self, db_conn):
+        self._conn = db_conn
+        self._table_name = "trade"
+        if not self._table_exist():
+            self._create_table()
+            self._conn.commit()
+
+    def append(self, t: model.Trade):
+        if not self._exist_row(t.bill_id):
+            self._insert_row(t)
+        else:
+            self._update_row(t)
+        self._conn.commit()
+
+    def get_last(self) -> model.Trade:
+        cur = self._conn.cursor()
+        sql = "SELECT bill_id, ccy, price, crypto FROM {} ORDER BY bill_id DESC LIMIT 1".format(
+            self._table_name,
+        )
+        for row in cur.execute(sql):
+            return model.Trade(bill_id=row[0], ccy=row[1], price=float(row[2]), crypto=float(row[3]))
+        else:
+            raise model.NoSuchRecord(sql=sql)
+
+    def query(self, last_bill_id: str):
+        cur = self._conn.cursor()
+        if last_bill_id is not None:
+            where_part = "WHERE bill_id > \"{}\"".format(last_bill_id)
+        else:
+            where_part = ""
+        sql = "SELECT bill_id, ccy, price, crypto FROM {} {} ORDER BY bill_id".format(self._table_name, where_part)
+        return [model.Trade(bill_id=row[0], ccy=row[1], price=float(row[2]), crypto=float(row[3])) for row in
+                cur.execute(sql)]
+
+    def _table_exist(self):
+        cur = self._conn.cursor()
+        sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='{}'".format(self._table_name)
+        for row in cur.execute(sql):
+            return True
+        else:
+            return False
+
+    def _create_table(self):
+        cur = self._conn.cursor()
+        sql = """
+        CREATE TABLE {} (
+            'bill_id' VARCHAR,
+            'ccy' VARCHAR,
+            'price' FLOAT,
+            'crypto' FLOAT
+        )
+        """.format(self._table_name)
+        print("create table sql=", sql)
+        for row in cur.execute(sql):
+            print("create table row>", row)
+
+    def _exist_row(self, bill_id: str) -> bool:
+        cur = self._conn.cursor()
+        sql = "SELECT bill_id FROM {} WHERE bill_id={} LIMIT 1".format(
+            self._table_name,
+            '"' + bill_id + '"'
+        )
+        for _ in cur.execute(sql):
+            return True
+        else:
+            return False
+
+    def _insert_row(self, t: model.Trade):
+        cur = self._conn.cursor()
+        sql = "INSERT INTO {} (bill_id, ccy, price, crypto) VALUES({}, {}, {}, {})".format(
+            self._table_name,
+            '"' + t.bill_id + '"',
+            '"' + t.ccy + '"',
+            t.price,
+            t.crypto,
+        )
+        return cur.execute(sql)
+
+    def _update_row(self, t: model.Trade):
+        cur = self._conn.cursor()
+        sql = "UPDATE {} SET ccy={}, price={}, crypto={}} WHERE bill_id={}".format(
+            self._table_name,
+            '"' + t.ccy + '"',
+            t.price,
+            t.crypto,
+            '"' + t.bill_id + '"',
+        )
+        return cur.execute(sql)
