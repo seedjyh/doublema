5cf7d78fb80a1fde497476e1df888f2951dca0e2
jyh <seedjyh@gmail.com>
2022-04-17 20:46:03 +0800

    simpler okex.api test

--- okex/test_api.py
+++ okex/test_api.py
@@ -5,7 +5,7 @@ from model import CCY_BTC, Candlestick, BAR_1D
 from okex._api import query_market_candles
 
 
-def test_query():
+def test_query_market_candles():
     res = query_market_candles(
         ccy=CCY_BTC, bar=BAR_1D,
         since=datetime(year=2022, month=4, day=1, hour=0, minute=0, second=0),
@@ -18,7 +18,7 @@ def test_query():
                                           h=43716.0, l=42112.0, c=42280.0).__dict__
 
 
-def test_query_partial():
+def test_query_market_candles_partial():
     res = query_market_candles(
         ccy=CCY_BTC, bar=BAR_1D,
         since=datetime(year=2022, month=3, day=31, hour=12, minute=0, second=0),
@@ -29,4 +29,3 @@ def test_query_partial():
                                           h=46707.8, l=44219.6, c=46529.6).__dict__
     assert res[8].__dict__ == Candlestick(t=datetime(year=2022, month=4, day=9), o=43649.3,
                                           h=43716.0, l=42112.0, c=42280.0).__dict__
-
